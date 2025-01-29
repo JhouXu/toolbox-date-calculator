98a70481f93815b3c583d25d959beb92206aed0b
jhouxu <2324837581@qq.com>
2025-01-29 11:21:25 +0800

    chore vant-UI

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,19 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import AutoImport from "unplugin-auto-import/vite";
+import Components from "unplugin-vue-components/vite";
+import { VantResolver } from "@vant/auto-import-resolver";
 
 // https://vite.dev/config/
 export default defineConfig({
   base: "/toolbox-date-calculator",
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    AutoImport({
+      resolvers: [VantResolver()],
+    }),
+    Components({
+      resolvers: [VantResolver()],
+    }),
+  ],
 });
